b4c438b691bfb8fb410d1efe9d352c88edbd3b05
dianagatcan <gatcan.diana@gmail.com>
2022-09-26 11:32:10 +0300

    resolve error from display function

--- script.js
+++ script.js
@@ -28,25 +28,22 @@ function select(elem) {
   //add selected class to element
   elem.classList.add(selectedClass);
 }
-
-//////
-
 function display(elem) {
-  if ((elem.innerHTML = "Speedy Searching")) {
+  if (elem.innerHTML === "Speedy Searching") {
     Array.from(featureSections)
       .find((featureSection) => featureSection.classList.contains(showSection))
       .classList.remove(showSection);
 
     //add selected class to element
     searchSection.classList.add(showSection);
-  } else if ((elem.innerHTML = "Simple Bookmarking")) {
+  } else if (elem.innerHTML === "Simple Bookmarking") {
     Array.from(featureSections)
       .find((featureSection) => featureSection.classList.contains(showSection))
       .classList.remove(showSection);
 
     //add selected class to element
     bookmarkSection.classList.add(showSection);
-  } else if ((elem.innerHTML = "Easy Sharing")) {
+  } else if (elem.innerHTML === "Easy Sharing") {
     Array.from(featureSections)
       .find((featureSection) => featureSection.classList.contains(showSection))
       .classList.remove(showSection);
